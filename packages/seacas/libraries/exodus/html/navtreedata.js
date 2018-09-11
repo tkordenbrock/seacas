c2262782bc814007eb068d710899a0ce1aa4ffb3
Greg Sjaardema <gsjaardema@gmail.com>
2018-09-11 10:11:34 -0600

    EXODUS: Redoing doxygen dirs to work better with Trilinos snapshot [ci skip]

---
+++ packages/seacas/libraries/exodus/html/navtreedata.js
@@ -0,0 +1,111 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Exodus", "index.html", [
+    [ "Exodus API Documentation", "index.html", [
+      [ "Introduction", "index.html#intro", null ],
+      [ "Availability", "index.html#avail", null ],
+      [ "License", "index.html#license", null ],
+      [ "Development of EXODUS", "index.html#devel", null ],
+      [ "Description of Data Objects", "index.html#descrip", null ],
+      [ "Integer Bulkdata Storage Details (32-bit and 64-bit integers)", "index.html#int64", [
+        [ "Fortran API", "index.html#int64_fortran_api", null ],
+        [ "Fortran Implementation", "index.html#int64_fortran_imp", null ]
+      ] ],
+      [ "Options (Compression, Name Length, File Type)", "index.html#Database", null ]
+    ] ],
+    [ "Exodus Element Types", "element_types.html", [
+      [ "Element Types", "element_types.html#types", null ],
+      [ "Exodus Node Ordering Convention", "element_types.html#ordering", null ],
+      [ "Circle and Sphere", "element_types.html#circle", null ],
+      [ "Bar, Truss, Beam", "element_types.html#bar", null ],
+      [ "Triangular", "element_types.html#tri", null ],
+      [ "Quadrilateral", "element_types.html#quad", null ],
+      [ "Tetrahedral", "element_types.html#tet", null ],
+      [ "Pyramidal", "element_types.html#pyr", null ],
+      [ "Wedge", "element_types.html#wedge", null ],
+      [ "Hexahedral", "element_types.html#hex", null ]
+    ] ],
+    [ "Exodus Formats", "exodus_formats.html", [
+      [ "Large Model (64-bit offset)", "exodus_formats.html#large", null ],
+      [ "Normal, or Classic", "exodus_formats.html#normal", null ],
+      [ "Netcdf-4 Classic", "exodus_formats.html#nc4", null ],
+      [ "Netcdf-4 Non-Classic", "exodus_formats.html#nc4nc", null ],
+      [ "CDF5", "exodus_formats.html#cdf5", null ]
+    ] ],
+    [ "Mapping of nemesis API functions to Exodus API functions", "nemesis-mapping.html", [
+      [ "Initial Information Routines", "nemesis-mapping.html#initial", null ],
+      [ "Loadbalance Parameter Routines", "nemesis-mapping.html#lb", null ],
+      [ "Nodeset, Sideset & Element Block Global Parameter Routines", "nemesis-mapping.html#param", null ],
+      [ "Nodeset, Sideset & Element Block Subset Routines", "nemesis-mapping.html#subset", null ],
+      [ "Variable Routines", "nemesis-mapping.html#variable", null ],
+      [ "Number Map Routines", "nemesis-mapping.html#map", null ],
+      [ "Communications Maps Routines", "nemesis-mapping.html#comm", null ]
+    ] ],
+    [ "Polyhedral Element Support", "polyhedra.html", [
+      [ "Storage of 3D arbitrary polyhedra elements in Exodus.", "polyhedra.html#poly", null ]
+    ] ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Undocumented", "undoc.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_2test_2CreateEdgeFace_8c-example.html",
+"ex__get__nodal__var__time_8c.html",
+"ex__put__map_8c.html",
+"exo__jack-windows_8c.html#a2afcad8a32ffecb9237121f272c06ad6",
+"exo__jack_8c.html#aada5339e7a153199d959e742bc8c253e",
+"exodusII_8h.html#aa18e08d59faa100114b77fe55900ec49",
+"exodusII__int_8h.html#a3633591cdb4ff17dccc9690b62da2fbf",
+"exodusII__int_8h.html#af598d5b32e831d8db9436f528d65d560",
+"group__ResultsData.html#ga90ac12b9e78cbb4305f07d27cffeb780"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
